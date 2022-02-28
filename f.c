399efe5b1be9eabe2f2d8a06a70c79cfa2e617c5
Igor Lipovtsev <igor.lipovcev@eltex.loc>
2022-02-28 12:13:22 +0700

    changed main.c & added f.c

---
+++ f.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void print_lib()
+{
+	printf ("%s\n", __FILE__);
+}
+
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
+#include "f.h"
 int main ()
 {
 	printf("qq all here");
+	print_lib();
 	return 0;
 }
